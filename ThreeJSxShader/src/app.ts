39d7114a1ad97ec736e734fec8431b74dadc9d93
momowu910 <momo520alice@gmail.com>
2022-12-22 11:25:10 +0800

    1_2範例程式

--- ThreeJSxShader/src/app.ts
+++ ThreeJSxShader/src/app.ts
@@ -1,8 +1,15 @@
 
-import ViewLession1_1_ans from '../lession1/viewLession1_1_ans';
+import ViewLession1_1 from '../lession1/viewLession1_1';
+// import ViewLession1_1_ans from '../lession1/viewLession1_1_ans';
+import ViewLession1_2 from '../lession1/viewLession1_2';
+// import ViewLession1_2_ans from '../lession1/viewLession1_2_ans';
+
 
 if (module.hot) {
     module.hot.accept();
 }
 
-new ViewLession1_1_ans();
+new ViewLession1_1();
+// new ViewLession1_1_ans();
+// new ViewLession1_2();
+// new ViewLession1_2_ans();
--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import View from './scene/view';
 
 import ViewLession1_1 from './scene/lession1/viewLession1_1';
 import ViewLession1_1_ans from './scene/lession1/viewLession1_1_ans';
+import ViewLession1_2 from './scene/lession1/viewLession1_2';
+import ViewLession1_2_ans from './scene/lession1/viewLession1_2_ans';
 
 if (module.hot) {
     module.hot.accept();
@@ -11,7 +13,9 @@ if (module.hot) {
 
 // new ViewReward();
 // new ViewWithPhysics();
-new View();
+// new View();
 
 // new ViewLession1_1();
 // new ViewLession1_1_ans();
+new ViewLession1_2();
+// new ViewLession1_2_ans();
