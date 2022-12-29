bb360962f6ce23024fb8fd319b08af02c1946d71
momowu910 <momo520alice@gmail.com>
2022-12-29 12:42:49 +0800

    補第二堂課東西

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,12 @@ import ViewLession1_1_ans from './scene/lession1/viewLession1_1_ans';
 import ViewLession1_2 from './scene/lession1/viewLession1_2';
 import ViewLession1_2_ans from './scene/lession1/viewLession1_2_ans';
 
+import ViewLession2_PerspectiveCamera from './scene/lession2/ViewLession2_PerspectiveCamera';
+import ViewLession2_OrthographicCamera from './scene/lession2/ViewLession2_OrthographicCamera';
+import ViewLession2_Scene from './scene/lession2/ViewLession2_Scene';
+import ViewLession2_Renderer from './scene/lession2/ViewLession2_Renderer';
+import ViewLession2_Mesh from './scene/lession2/ViewLession2_Mesh';
+
 if (module.hot) {
     module.hot.accept();
 }
@@ -17,5 +23,11 @@ if (module.hot) {
 
 // new ViewLession1_1();
 // new ViewLession1_1_ans();
-new ViewLession1_2();
+// new ViewLession1_2();
 // new ViewLession1_2_ans();
+
+// new ViewLession2_PerspectiveCamera(); // 透視相機
+// new ViewLession2_OrthographicCamera(); // 正交相機
+// new ViewLession2_Scene(); // 場景
+// new ViewLession2_Renderer(); // 渲染器
+new ViewLession2_Mesh(); // 物件、材質、形狀
