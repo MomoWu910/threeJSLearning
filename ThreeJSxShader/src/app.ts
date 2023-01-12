035012f3fc59dd0762590a7b5733ee3f1884fd30
EricWu <eric.wu@skylinetw.com>
2023-01-12 11:09:18 +0800

    merge: lession 2

--- ThreeJSxShader/src/app.ts
+++ ThreeJSxShader/src/app.ts
@@ -5,11 +5,24 @@ import ViewLession1_2 from '../lession1/viewLession1_2';
 // import ViewLession1_2_ans from '../lession1/viewLession1_2_ans';
 
 
+import ViewLession2_PerspectiveCamera from '../lession2/ViewLession2_PerspectiveCamera';
+import ViewLession2_OrthographicCamera from '../lession2/ViewLession2_OrthographicCamera';
+import ViewLession2_Scene from '../lession2/ViewLession2_Scene';
+import ViewLession2_Renderer from '../lession2/ViewLession2_Renderer';
+import ViewLession2_Mesh from '../lession2/ViewLession2_Mesh';
+
+
 if (module.hot) {
     module.hot.accept();
 }
 
-new ViewLession1_1();
+// new ViewLession1_1();
 // new ViewLession1_1_ans();
 // new ViewLession1_2();
 // new ViewLession1_2_ans();
+
+// new ViewLession2_PerspectiveCamera(); // 透視相機
+// new ViewLession2_OrthographicCamera(); // 正交相機
+// new ViewLession2_Scene(); // 場景
+// new ViewLession2_Renderer(); // 渲染器
+new ViewLession2_Mesh(); // 物件、材質、形狀
